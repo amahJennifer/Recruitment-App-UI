a83f91bad1c5804c179bbc1803fad74394e2e38c
amahJennifer <jennifer000@rocketmail.com>
2020-06-15 19:08:32 +0100

    Add Tech Section

--- src/layout/content/index.js
+++ src/layout/content/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from "../../pages/home/home";
 import Steps from "../../pages/steps"
 import Talent from "../../pages/talent"
+import Tech from "../../pages/tech"
 const index = () => {
   return (
     <>
       <Home />
       <Steps />
-      <Talent/>
+      <Talent />
+      <Tech/>
     </>
   )
 }
---
+++ src/pages/tech/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./index.css";
+const index = () => {
+  return (
+    <div className="techContainer">
+      <div><h3>Are you a tech Professional?</h3>
+<h3>Do you need a Job?</h3></div>
+<div className="helpContainer">
+   <button className="helpBtn">
+       Lets Help you
+       </button>
+  </div>
+    
+    </div>
+  )
+}
+
+export default index
+
+
+
+
